07b789f2e6d19e5110782a028abb44740b5c06c1
Jacob <Bracket0723@gmail.com>
2025-11-21 00:11:48 +0900

    ✨ Add ProfileCard composable

--- app/src/main/java/kr/yooreka/jafar/MainActivity.kt
+++ app/src/main/java/kr/yooreka/jafar/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.yooreka.jafar.ui.theme.JafarTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +41,55 @@ class MainActivity : ComponentActivity() {
         setContent {
             JafarTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Row(modifier = Modifier.padding(innerPadding)) {
+                        ProfileCard()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ProfileCard() {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "프로필 사진",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(80.dp),
+            )
+            Text(
+                stringResource(R.string.name)
+            )
+            Text(
+                stringResource(R.string.job)
+            )
+            val skills = stringArrayResource(id = R.array.skills)
+            Row(
+            ) {
+                skills.forEach { item ->
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Text(
+                        text = item,
+                        Modifier
+                            .background(Color.LightGray, RoundedCornerShape(4.dp))
+                            .padding(start = 8.dp, end = 8.dp)
                     )
+                    Spacer(modifier = Modifier.size(4.dp))
                 }
             }
         }
@@ -31,17 +97,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Home() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        ProfileCard()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ProfilePreview() {
     JafarTheme {
-        Greeting("Android")
+        ProfileCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    JafarTheme {
+        Home()
     }
 }
